e10aa504dd6c04486be71ab7cd5aac80a3a64368
TobyVB <98196156+TobyVB@users.noreply.github.com>
2023-03-07 12:48:27 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
