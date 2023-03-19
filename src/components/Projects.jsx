b2b83262ff7ae8ce3683473bb89eb1fc719e8fef
TobyVB <98196156+TobyVB@users.noreply.github.com>
2023-03-19 16:57:15 +0800

    5 total projects data added

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -8,10 +8,38 @@ export default function Projects() {
       github: "https://github.com/TobyVB/Talkspace",
       demo: "https://fir-practice-cace4.web.app/",
     },
-    // { path: "", size: "200%", title: "", info: "", github: "", demo: "" },
-    // { path: "", size: "200%", title: "", info: "", github: "", demo: "" },
-    // { path: "", size: "200%", title: "", info: "", github: "", demo: "" },
-    // { path: "", size: "200%", title: "", info: "", github: "", demo: "" },
+    {
+      path: "https://user-images.githubusercontent.com/98196156/221128205-2bbbedc7-b11a-4072-9a97-baa6f2c300cc.jpg",
+      size: "40%",
+      title: "SuperChat",
+      info: "SuperChat is a chatroom web app that utilizes google authentication. I completed this project as a code-along by Fireship",
+      github: "https://github.com/TobyVB/SuperChat",
+      demo: "https://superchat-aa35f.web.app/",
+    },
+    {
+      path: "https://user-images.githubusercontent.com/98196156/221082488-c0eec32b-a125-489c-9857-64e5014e9118.jpg",
+      size: "40%",
+      title: "Quiz App",
+      info: "This Quiz web app uses an API provided from the Open Trivia website. Users can filter questions by count, type, category, and difficulty. Users are then scored by number of correct selected answers.",
+      github: "https://github.com/TobyVB/Quiz-App",
+      demo: "https://quiz-app-29f85.web.app/",
+    },
+    {
+      path: "https://user-images.githubusercontent.com/98196156/226163163-48309ea3-a83e-45c9-983d-8087bc28e2b0.png",
+      size: "100%",
+      title: "Movie DB App",
+      info: "This movie database web app uses an API provided from the The Movie Database website.",
+      github: "https://github.com/TobyVB/movieDB",
+      demo: "https://quiz-app-29f85.web.app",
+    },
+    {
+      path: "https://tobyvbsblog.vercel.app/",
+      size: "100%",
+      title: "Blog App",
+      info: "This Blog web app was made by following the NextJS documentation",
+      github: "https://github.com/TobyVB/Blog/edit/main/README.md",
+      demo: "https://tobyvbsblog.vercel.app/",
+    },
   ];
 
   function ProjectsArray() {
